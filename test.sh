c96bf7ae10073b52f5cc754b7858ce9348ed50e5
liufengchengxing <liufengchengxing@outlook.com>
2025-08-18 05:37:11 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -511,7 +511,9 @@ new_config = '''config = {
                 {
                     "type": "field",
                     "domain": [
-                        "youtube.com", "youtu.be","telegram.org","t.me",
+                        "youtube.com", "youtu.be",
+                        "telegram.org",
+                        "t.me",
                         "googlevideo.com",
                         "ytimg.com",
                         "gstatic.com",
